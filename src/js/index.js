68324be4c75d392550d581c80e1ff00a1c75ac67
Alen Makaric <alen.makaric@etondigital.com>
2020-03-26 15:59:41 +0100

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 //  Global state of the app
 // - Search object
@@ -21,12 +21,14 @@ const controlSearch = async () => {
         // 3) Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // 4) Search for recipes
         await state.search.getResults();
 
         // 5) Render results on UI
-        console.log(state.search.result);
+        clearLoader();
+        // console.log(state.search.result);
         searchView.renderResults(state.search.result);
         
     }
@@ -35,6 +37,19 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
+})
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn =e.target.closest('.btn-inline');
+    // console.log(btn);
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+        console.log(goToPage);
+        
+    }
+    
 })
 
 // const search = new Search('pizza');
